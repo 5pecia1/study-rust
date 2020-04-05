8314e9e2ab43ca13e3de701380ca8342af44da41
5pecia1 <pdpxpd@gmail.com>
2020-04-05 19:50:04 +0900

    study 3.4

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/sol/go/src/github.com/5pecia1/study-rust/branches/target/debug/branches: /Users/sol/go/src/github.com/5pecia1/study-rust/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-f2078a2590e78b24.d
@@ -0,0 +1,5 @@
+/Users/sol/go/src/github.com/5pecia1/study-rust/branches/target/debug/deps/branches-f2078a2590e78b24: src/main.rs
+
+/Users/sol/go/src/github.com/5pecia1/study-rust/branches/target/debug/deps/branches-f2078a2590e78b24.d: src/main.rs
+
+src/main.rs:
